c05e082ecd9e2013e9f354adb85fd21d3b008802
Artem <Artem>
2021-07-03 23:39:07 +0300

    Added a button to add new income or expense

---
+++ Itis-FinApp/MainScreen/MainScreenAddViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainScreenAddViewController.swift
+//  Itis-FinApp
+//
+//  Created by Artem Kalugin on 03.07.2021.
+//
+
+import UIKit
+
+class MainScreenAddViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
