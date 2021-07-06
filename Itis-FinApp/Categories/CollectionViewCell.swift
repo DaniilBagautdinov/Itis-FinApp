c9e8a18c21f5ce449d50fb6ed6513d5aa817610f
Semen Sokolov <simionsimion@yandex.ru>
2021-07-06 13:46:02 +0300

    Categories viewController and CollectionViewCell that contains picture and name of category (storyboard with some features) and library that help me to draw a pie chart (example pie chart without data included)

---
+++ Itis-FinApp/Categories/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Itis-FinApp
+//
+//  Created by Семен Соколов on 06.07.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
